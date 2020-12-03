a9df137886c0f9cf66df82f379105d7f92ef4e62
Fabrício Augusto <fabricio.mata@gmail.com>
2020-12-02 23:57:37 -0300

    fix: remove middleware in get products

--- src/modules/products/routes/product.routes.ts
+++ src/modules/products/routes/product.routes.ts
@@ -5,10 +5,9 @@ import ProductController from '@modules/products/controllers/product-controller'
 const productRouter = Router()
 const productController = new ProductController()
 
-productRouter.use(isAdminAuth)
 productRouter.get('/', productController.index)
-productRouter.post('/', productController.create)
-productRouter.put('/', productController.update)
+productRouter.post('/', isAdminAuth, productController.create)
+productRouter.put('/', isAdminAuth, productController.update)
 productRouter.get('/i/:id', productController.show)
 productRouter.get('/s/:slug', productController.show)
 
